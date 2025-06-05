68ad5902bc9158e61443a6a91ecb91913cc0bdf7
GOKUL's ADDA <pgokulsree1116@gmail.com>
2025-06-05 09:34:57 +0000

    i want it in the background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import ContactSection from '@/components/sections/contact-section';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative z-10">
       <Navbar />
       <main className="flex-grow">
         <LandingSection />
