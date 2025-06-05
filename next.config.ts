7080828b73478bc549ddfe2746dc2c1556e24ec5
GOKUL's ADDA <pgokulsree1116@gmail.com>
2025-06-05 09:56:01 +0000

    the image is not visible

--- next.config.ts
+++ next.config.ts
@@ -22,7 +22,7 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: true, // Add this if you are serving local images and don't need optimization
+    unoptimized: false, // Changed from true
   },
   experimental: {
     allowedDevOrigins: ['9000-firebase-studio-1749112787293.cluster-ys234awlzbhwoxmkkse6qo3fz6.cloudworkstations.dev'],
